df627b9a2bf112b22e805b55f1d819d22ae20cf4
pooja <pooja06bhosale@gmail.com>
2024-12-06 14:09:25 +0530

    oo

--- src/main/java/com/example/demo/SayHello.java
+++ src/main/java/com/example/demo/SayHello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SayHello {
 
-        @RequestMapping("/hello")
+        @RequestMapping("/hellooo)
         public String SayHello() {
             return "Hello";
 
